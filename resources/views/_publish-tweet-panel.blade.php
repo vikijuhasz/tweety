a96be11ea6413ac41ac9882798b348ef45a0d410
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-04-22 09:00:31 +0200

    design the timeline

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,20 @@
+<div class="border-2 border-blue-300 rounded-lg px-8 py-6 mb-8">
+    <form action="">
+        <textarea 
+            name="body"
+            class="w-full"
+            placeholder="What's up doc?">
+        </textarea>
+
+        <hr class="py-4">
+
+        <footer class="flex justify-between">
+            <img 
+            src="/images/image-1.png" 
+            alt=""
+            class="rounded-full mr-2"
+            >
+            <button type="submit" class="bg-blue-500 rounded-lg shadow py-2 px-2 text-white">Tweet-a-roo!</button>
+        </footer>
+    </form>
+</div>
