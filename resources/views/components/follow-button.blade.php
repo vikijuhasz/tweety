381202fd97e136d6e8720a428d6d9bc6557d979c
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-04-24 11:38:27 +0200

    profile authorization

--- resources/views/components/follow-button.blade.php
+++ resources/views/components/follow-button.blade.php
@@ -1,6 +1,8 @@
+@unless (current_user()->is($user))
 <form method="POST" action="/profiles/{{ $user->name }}/follow">
     @csrf
     <button type="submit" class="bg-blue-500 rounded-full shadow py-2 px-2 text-white text-xs">
-        {{ auth()->user()->following($user) ? 'Unfollow Me' : 'Follow Me' }}
+        {{ current_user()->following($user) ? 'Unfollow Me' : 'Follow Me' }}
     </button>
 </form>
+@endunless
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -23,7 +23,9 @@
          </div>
 
          <div class="flex">
-            <a href="" class="rounded-full border border-gray-300 py-2 px-2 text-black text-xs mr-2">Edit Profile</a>
+            @can('edit', $user)
+               <a href="{{ $user->path('edit') }}" class="rounded-full border border-gray-300 py-2 px-2 text-black text-xs mr-2">Edit Profile</a>
+            @endcan
             
             <x-follow-button :user="$user"></x-follow-button>
 
