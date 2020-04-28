735643fd63f68ace1d408295cff442731e6dee10
Viktoria Juhasz <viktoriajuhasz8@gmail.com>
2020-04-28 18:40:18 +0200

    like/dislike system

--- app/User.php
+++ app/User.php
@@ -53,12 +53,18 @@ class User extends Authenticatable
         
         return Tweet::whereIn('user_id', $friends)
             ->orWhere('user_id', $this->id)
+            ->withLikes()
             ->latest()->paginate(15);
     }
 
     public function tweets()
     {
         return $this->hasMany(Tweet::class)->latest();
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 
     public function path($append = '')
--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -32,5 +32,7 @@
         
         {{ $slot }}
     </div>
+
+    <script src="http://unpkg.com/turbolinks"></script>
 </body>
 </html>
